533ce352ba94a49da5f370b1f66cd2f9f96ec20e
Unknown <methalicity@gmail.com>
2018-07-07 14:39:43 +0200

    TitleAPI

--- src/main/java/cz/ignissak/fbr/utils/TitleAPI.java
+++ src/main/java/cz/ignissak/fbr/utils/TitleAPI.java
@@ -1,6 +1,10 @@
 package cz.ignissak.fbr.utils;
 
+import net.minecraft.server.v1_12_R1.IChatBaseComponent;
+import net.minecraft.server.v1_12_R1.PacketPlayOutTitle;
+import net.minecraft.server.v1_12_R1.PlayerConnection;
 import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class TitleAPI {
